40d93743075239892a5e343191753457814e1599
jaredes291 <jaredes291@gmail.com>
2022-07-19 02:14:10 -0600

    added code comments for the main window

--- BFM1 Task1/MainWindow.xaml.cs
+++ BFM1 Task1/MainWindow.xaml.cs
@@ -18,17 +18,19 @@ namespace Task1.UI
             //Startup
             InitializeComponent();
             DataContext = Inventory;
+            //test data
             Inhouse test1 = new Inhouse("hydrocoptic marzelvanes", 36.55M, 25, 1, 25, 35);
             Inhouse test2 = new Inhouse("lunar waneshaft", 36.55M, 25, 1, 25, 35);
             Inhouse test3 = new Inhouse("tremie pipe", 36.55M, 25, 1, 25, 35);
             Inhouse test4 = new Inhouse("grammeters", 36.55M, 25, 1, 25, 35);
             Product test5 = new Product("Turbo Encabulator", 36.55M, 25, 1, 25);
-
+            //add test data
             Inventory.addPart(test1);
             Inventory.addPart(test2);
             Inventory.addPart(test3);
             Inventory.addPart(test4);
             Inventory.addProduct(test5);
+
             //refresh datacontext
             refresh();
         }
@@ -41,13 +43,26 @@ namespace Task1.UI
             DataContext = null;
             DataContext = Inventory;
         }
-
+        /// <summary>
+        /// event handler for the button "Add" part.
+        /// creates a PartManagement window for the user create a new part.
+        /// if the user cancels the Add action then no part will be added.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPartAdd_Click(object sender, RoutedEventArgs e)
         {
             PartManagement addPart = new PartManagement();
             addPart.ShowDialog();
         }
-
+        /// <summary>
+        /// event handler for the modify part button
+        /// creates a backup of the target part then creates a PartManagement window for the user 
+        /// to make changes to the selected part. if the user cancels the modify action the modified 
+        /// part will be replaced with the backup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPartModify_Click(object sender, RoutedEventArgs e)
         {
             Part target = (Part)PartsRoster.SelectedItem;
@@ -80,6 +95,12 @@ namespace Task1.UI
                 MessageBox.Show($"Please select a Part.", "Select Part", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
+        /// <summary>
+        /// event handler for the delete part button
+        /// deletes a selected part from the part list and confirms the action with the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPartDelete_Click(object sender, RoutedEventArgs e)
         {
             Part target = (Part)PartsRoster.SelectedItem;
@@ -102,7 +123,13 @@ namespace Task1.UI
                 MessageBox.Show($"Please select a Part.", "Select Part", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
-
+        /// <summary>
+        /// event handler for the add product button
+        /// creates a ProductManagement window for the user create a new product.
+        /// if the user cancels the Add action then no product will be added.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnProductAdd_Click(object sender, RoutedEventArgs e)
         {
             ProductManagement productManagement = new ProductManagement(Inventory);
@@ -110,7 +137,14 @@ namespace Task1.UI
             Product tempProduct = productManagement.Product;
             Inventory.Products.Add(tempProduct);
         }
-
+        /// <summary>
+        /// event handler for the modify button
+        /// creates a backup of the target product then creates a ProductManagement window for the user 
+        /// to make changes to the selected product. if the user cancels the modify action the modified 
+        /// product will be replaced with the backup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnProductModify_Click(object sender, RoutedEventArgs e)
         {
             Product target = (Product)ProductsRoster.SelectedItem;
@@ -147,7 +181,12 @@ namespace Task1.UI
                 MessageBox.Show($"Please select a Product.", "Select Product", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
-
+        /// <summary>
+        /// event handler for the delete product button
+        /// deletes a selected product from the products list and confirms the action with the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnProductDelete_Click(object sender, RoutedEventArgs e)
         {
             Product target = (Product)ProductsRoster.SelectedItem;
@@ -170,12 +209,28 @@ namespace Task1.UI
                 MessageBox.Show($"Please select a Product.", "Select Product", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
-
+        /// <summary>
+        /// event handler for the part search box
+        /// sets the item source for the part ListBox to a filtered list that matches the search box string
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void PartSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) => PartsRoster.ItemsSource = Inventory.Parts.Where(em => em.Name.ToLower().Contains(PartSearch.Text.ToLower()));
+        /// <summary>
+        /// event handeler for the product search box
+        /// sets the item source for the product ListBox to a filtered list that matches the search box string
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void ProductSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) => ProductsRoster.ItemsSource = Inventory.Products.Where(em => em.Name.ToLower().Contains(ProductSearch.Text.ToLower()));
-
+        /// <summary>
+        /// event handler for the exit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
+            //close the program
             Close();
         }
     }
